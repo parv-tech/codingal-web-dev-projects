32f5f86b83f5d7172eaf7f46b8a2861edb14c493
parv-tech <gillpreeti1988@gmail.com>
2025-09-06 08:25:22 +0100

    Add files via upload

---
+++ natural number test python/natural.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number to check if it is a natural number or not : "))
+
+if num < 0 :
+    print("Your number is not a natural number . ")
+
+elif num > 0 :
+    print("Your number is a natural number . ")   
+
+elif num == 0 :
+    print("Your number is nuetral number .  ")     
+
