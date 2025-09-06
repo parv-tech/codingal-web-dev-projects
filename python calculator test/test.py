6846842487ed79d9827091a1210d9ee00429d492
parv-tech <gillpreeti1988@gmail.com>
2025-09-06 10:38:42 +0100

    Add files via upload

---
+++ python calculator test/test.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter a number : "))
+
+num2 = int(input("Enter another number :"))
+
+sign = str(input("Enter a sign in words : ")) 
+
+ 
+if (sign == "plus") :
+    a = num1 + num2
+    print(a)
+
+elif (sign == "minus") :
+    b = num1 - num2
+    print(b)
+
+elif (sign == "multiply") : 
+    c = num1 * num2
+    print(c)
+
+elif (sign == "divide") : 
+    d = num1 / num2
+    print(d)
+
+    
+    
+    
