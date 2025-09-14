14c5345f5f14de6b8262440947b3ec35c98b8710
parv-tech <gillpreeti1988@gmail.com>
2025-09-14 10:17:20 +0100

    Add files via upload

---
+++ Object oriented programming part 1/opp .py
@@ -0,0 +1,7 @@
+class fruits : 
+    def __init__ (self , name , colour) :
+        self.name = name
+        self.colour = colour
+yay = fruits("Banana , apple" , "Yellow , red")
+print(yay.name)
+print(yay.colour)
